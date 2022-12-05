78fc7226d708fb369f42ea427416cf73bc8c85c1
Caio Nunes <caioayslan2@gmail.com>
2022-12-05 00:24:30 -0300

    add: foi adicionado o projeco countdown

--- src/api/data.ts
+++ src/api/data.ts
@@ -1,33 +1,32 @@
-import imageCardDefault from '../../public/teste.png'
-import imageSocialTree from '../../public/social-tree-img.png'
-import imageSearchCep from '../../public/search-cep-img.png'
-
+import imageSocialTree from '../../public/social-tree-img.png';
+import imageSearchCep from '../../public/search-cep-img.png';
+import imageCountdown from '../../public/countdown.png';
 
 const data = [
-    {
-        id: 1,
-        sourceImgCard: imageSocialTree,
-        titleCard: "Social Tree",
-        descriptionCard: "Esse projeto se resume em uma pagina no qual consta as minhas principais redes sociais, o mesmo tem o objetivo de reunir todas em um unico local.",
-        linkDeployCard: "https://github.com/caio-nuness",
-        linkCodeCard: "https://github.com/caio-nuness/socials",
-    },
-    {
-        id: 2,
-        sourceImgCard: imageSearchCep,
-        titleCard: "Segundo Projeto",
-        descriptionCard: "O melhor buscador de cep da região. Faça suas buscas com rapidez, e especificidade e segurança, entre com o cep e consiga diversos outros dados do endereço.",
-        linkDeployCard: "https://github.com/caio-nuness",
-        linkCodeCard: "https://github.com/caio-nuness/search-cep",
-    },
-    {
-        id: 3,
-        sourceImgCard: imageCardDefault,
-        titleCard: "Produzindo...",
-        descriptionCard: "O melhor buscador de cep da região. Faça suas buscas com rapidez, e especificidade e segurança, entre com o cep e consiga diversos outros dados do endereço.",
-        linkDeployCard: "https://github.com/caio-nuness",
-        linkCodeCard: "https://github.com/caio-nuness/search-cep",
-    },
-]
+  {
+    id: 1,
+    sourceImgCard: imageSocialTree,
+    titleCard: 'Social Tree',
+    descriptionCard: 'Esse projeto se resume em uma pagina no qual consta as minhas principais redes sociais, o mesmo tem o objetivo de reunir todas em um unico local.',
+    linkDeployCard: 'https://github.com/caio-nuness',
+    linkCodeCard: 'https://github.com/caio-nuness/socials',
+  },
+  {
+    id: 2,
+    sourceImgCard: imageSearchCep,
+    titleCard: 'SearchCep',
+    descriptionCard: 'O melhor buscador de cep da região. Faça suas buscas com rapidez, e especificidade e segurança, entre com o cep e consiga diversos outros dados do endereço.',
+    linkDeployCard: 'https://github.com/caio-nuness',
+    linkCodeCard: 'https://github.com/caio-nuness/search-cep',
+  },
+  {
+    id: 3,
+    sourceImgCard: imageCountdown,
+    titleCard: 'Countdown',
+    descriptionCard: 'Página de lançamento de site ou páginas web. Com esse projeto você pode gerar mais expectativa do seu publico quanto a sua página, blog garantindo o seu engajamento',
+    linkDeployCard: 'https://github.com/caio-nuness',
+    linkCodeCard: 'https://github.com/caio-nuness/coutdown',
+  },
+];
 
 export default data;
